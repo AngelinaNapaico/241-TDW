c9002adb7077eff897f8a6922cf93891006ac5a8
Angelina Napaico Valencia <angelina.napaico.valencia@vallegrande.edu.pe>
2026-04-30 19:08:16 +0000

    feat: implementation of contact form with validation and connection to mysql

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
 from flask_mysqldb import MySQL
-import requests # type: ignore
-# inicializacion de mi frameword flask
-app = Flask(__name__)
-# configuracion de mysql
-mysql=MySQL(app)
-app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_PORT']=3306
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='123456'
-app.config['MYSQL_DB']='contactanos_db'
-mysql.init_app(app)
 
-@app.route('/' , methods=['GET', 'POST'])
+# Inicialización de Flask
+app = Flask(__name__)
+
+# CONFIGURACIÓN NECESARIA PARA FLASH (Añade esta línea)
+app.secret_key = 'mi_llave_secreta_provisional'
+
+# Configuración de MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'contactanos_db'
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':            
         name = request.form['name']
         email = request.form['email']
         message = request.form['message']
 
- # Aqui puedes agregar la logica para guardar los datos en la base de datos
-    cur = mysql.connection.cursor()
-    mysql.connection.commit()
-    cur.close()
-    flash('Contacto guardado exitosamente!')
+        # Conexión y ejecución del CRUD
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO usuarios (name, email, message) VALUES (%s, %s, %s)", (name, email, message))
+        mysql.connection.commit()
+        cur.close()
+        
+        flash('¡Contacto guardado exitosamente!')
+        return render_template('contact.html') 
+        
     return render_template('contact.html')
-# ejecutar mi servidor
+
+# Ejecutar servidor
 if __name__ == '__main__':
     app.run(debug=True)
